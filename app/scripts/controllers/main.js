e45efe9f4fa120f8c99010c66b81b75433cc1265
BOSS-PC\boss <contact@eplex.eu>
2017-04-11 09:08:42 +0200

    poczatek

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zadanieApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zadanieApp
+ */
+angular.module('zadanieApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
